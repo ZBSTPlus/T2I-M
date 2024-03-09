9734e2cf7a46bbb2c0dd7e464cdefaa8e0b44ccd
Mubashira31 <liferacer1718@gmail.com>
2024-03-09 09:01:02 +0530

    new commit

---
+++ text_image/text_image/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+from django.contrib import admin
+
+urlpatterns = [
+     path('admin/',admin.site.urls),
+         path('',main,name='main'),
+    path('index/',index, name='index'), 
+     # URL pattern for the main page
+    # Add more URL patterns as needed   
+    path('download-image/', download_image, name='download_image'),
+
+]
