86060e8ef6c38c1dc3cb6c4f341e9b72ef3cdbe0
Mubashira31 <liferacer1718@gmail.com>
2024-03-30 13:31:40 +0530

    error solved

--- text_image/text_image/myapp/text_to_image_converter.py
+++ text_image/text_image/myapp/text_to_image_converter.py
@@ -3,11 +3,13 @@ import os
 from io import BytesIO
 import base64
 from pathlib import Path
-def text_to_image(text,font_size="24",font_color="black",font_family="Arial", background_color="#FFFFFF",image_path="output.png"):
-    font_size=int(font_size)
+
+
+def text_to_image(text, font_size="24", font_color="black", font_family="Arial", background_color="#FFFFFF", image_path="output.png"):
+    font_size = int(font_size)
     image = Image.new("RGB", (500, 200), background_color)
     draw = ImageDraw.Draw(image)
-    font_path=""
+    font_path = ""
     if font_family == "Arial":
         font_path = "arial.ttf"
     elif font_family == "Times New Roman":
@@ -16,16 +18,18 @@ def text_to_image(text,font_size="24",font_color="black",font_family="Arial", ba
         font_path = "FORTE.TTF"
     else:
         font_path = "arial.ttf"
-    font_path = Path.joinpath(Path(__file__).parent,font_path)
-    font = ImageFont.truetype(font_path, font_size)  
+    font_path = Path.joinpath(Path(__file__).parent, font_path)
+    font = ImageFont.truetype(str(font_path), font_size)
     # text_width, text_height = draw.textsize(text, font)
     # text_x = (image.width - text_width) // 2
     # text_y = (image.height - text_height) // 2
 
     draw.text((100, 100), text, fill=font_color, font=font)
-    
+
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue())
+    # img_str = ImageFont.truetype(str(img_str), 15)
+
     image.save(image_path)
-    return  img_str.decode("utf-8")
+    return img_str.decode("utf-8")
