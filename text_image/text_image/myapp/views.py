6ab38022d44ccf338e03510c2cc9d6560ea8a2f0
Mubashira31 <liferacer1718@gmail.com>
2024-03-30 20:14:04 +0530

    fixed download button

--- text_image/text_image/myapp/views.py
+++ text_image/text_image/myapp/views.py
@@ -14,7 +14,7 @@ def index(request):
         font_family = request.POST.get('font_family', 'Arial')
 
         # custom_image_path = Path('C:/Users/HP/Desktop/text_image/text_image/text_image/myapp/static/images/output.png')
-        custom_image_path = Path.joinpath(Path(__file__).parent,"output.png")
+        custom_image_path = Path.joinpath(Path(__file__).parent, "output.png")
         print(custom_image_path)
         image_path = text_to_image(
             input_text, font_size, font_color, font_family, background_color, custom_image_path)
@@ -24,8 +24,7 @@ def index(request):
 
 def download_image(request):
 
-    image_path = Path(
-        'C:/Users/HP/Desktop/text_image/text_image/text_image/myapp/static/images/output.png')
+    image_path = Path.joinpath(Path(__file__).parent, "output.png")
     if image_path.exists():
         with open(image_path, 'rb') as file:
             response = HttpResponse(file.read(), content_type='image/png')
